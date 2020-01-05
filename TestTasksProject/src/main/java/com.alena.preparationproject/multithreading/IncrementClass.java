aa2f013381f742cf6dea8fa5a59468b604ac70e6
alenchik <alena-kov-1992@mail.ru>
2020-01-05 14:36:13 +0400

    clean repository

--- TestTasksProject/src/main/java/com.alena.preparationproject/multithreading/IncrementClass.java
+++
@@ -1,8 +0,0 @@
-package com.alena.preparationproject.multithreading;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-interface IncrementClass extends ThreadFunction<Integer> {
-    int incrementAndGet();
-    int get();
-}
--- TestTasksProject/src/main/java/com.alena.preparationproject/multithreading/ThreadFunction.java
+++
@@ -1,6 +0,0 @@
-package com.alena.preparationproject.multithreading;
-
-public interface ThreadFunction<T> {
-    T doTask(T value);
-    T getResult();
-}
